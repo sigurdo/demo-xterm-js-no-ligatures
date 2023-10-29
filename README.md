9ee6757f47b2ba8b217765bdd225b88907b89255
Sigurd Tullander <2935944-sigurdo@users.noreply.gitlab.com>
2023-10-29 11:19:32 +0100

    Implement demonstration

--- README.md
+++ README.md
@@ -9,7 +9,9 @@ bun install
 To run:
 
 ```bash
-bun run index.js
+python3 -m http.server
 ```
+
+then open [localhost:8000/index.html](localhost:8000/index.html) in your browser.
 
 This project was created using `bun init` in bun v1.0.7. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Hello via Bun!");
+import { Terminal } from 'xterm';
+
+// let resolution = [30000, 15000];
+let font_size = 20;
+let columns = 140;
+let rows = 70;
+
+// let font_size = Math.floor(resolution[1] / rows);
+let resolution = [font_size / 2 * columns, font_size * rows];
+
+let terminal_element = document.querySelector('#terminal');
+terminal_element.style.width = resolution[0] + 'px';
+terminal_element.style.height = resolution[1] + 'px';
+let terminal = new Terminal({
+    fontSize: font_size,
+    fontFamily: "Fira Code",
+    theme: {
+        background: '#272822'
+    },
+    cols: columns,
+    rows: rows,
+    convertEol: true,
+    disableStdin: true,
+});
+console.log(terminal.cols);
+terminal.open(terminal_element);
+// Hide terminal cursor
+terminal.write('\x1b[?25l');
+terminal.write('===');
